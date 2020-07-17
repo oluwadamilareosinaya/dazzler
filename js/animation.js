1805ece35a09a70db237f1ae61d3aa468034993f
RenegadeGandhi <osinayaoludare@gmail.com>
2020-07-17 02:21:12 -0600

    heroku v11

--- js/animation.js
+++ js/animation.js
@@ -1,1 +1,8 @@
-gsap.from(".navbar-brand", {duration: 2, opacity: 0.5, x: -200});
+
+const tl = gsap.timeline({defaults: {duration: 1}});
+
+tl.from(".navbar-brand", {opacity: 0.5, x: -300, ease: "bounce"})
+    .from(".hero-image", {scale: 0.3, opacity: 0.2, ease: "slow"}, "-=0.7")
+    .from(".hero-text h1", {x: 700, ease: "slow"}, "-=0.2")
+    .from(".hero-text p", {x: 700, ease: "slow"}, "-=0.2")
+    .from("form", {opacity: 0, scale: 0.2})
